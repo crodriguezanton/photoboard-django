baf6118e4b1f2c5a00cffca0629cca29af8b0d16
Carlos Rodríguez <c.rodriguezanton@gmail.com>
2016-10-05 17:39:34 +0200

    API changes

--- api/routers.py
+++ api/routers.py
@@ -1,6 +1,8 @@
 from rest_framework import routers
 
-from api.viewsets import UserViewSet
+from api.viewsets import UserViewSet, PictureViewSet, StudentViewSet
 
 default_router = routers.DefaultRouter()
 default_router.register(r'users', UserViewSet)
+default_router.register(r'students', StudentViewSet)
+default_router.register(r'pictures', PictureViewSet)
--- api/viewsets.py
+++ api/viewsets.py
@@ -2,9 +2,21 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 
-from api.serializers import UserSerializer
+from api.serializers import UserSerializer, PictureSerializer, StudentSerializer
+from pictures.models import Picture
+from education.models import Student
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class PictureViewSet(viewsets.ModelViewSet):
+    queryset = Picture.objects.all()
+    serializer_class = PictureSerializer
