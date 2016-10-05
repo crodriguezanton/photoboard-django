b1043015d7d6bc4d59808889ccf66edf82c251e4
Carlos Rodríguez <c.rodriguezanton@gmail.com>
2016-10-05 17:39:42 +0200

    Serializer changes

--- api/serializers.py
+++ api/serializers.py
@@ -2,8 +2,23 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from education.models import Student
+from pictures.models import Picture
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+class StudentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Student
+        fields = ('url', 'user')
+
+
+class PictureSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Picture
+        fields = ('url', 'picture', 'student')
