0baf643a63b10553ce1503f91b242cfb7c40b87b
Carlos Rodríguez <carlos@beatbcn.com>
2016-11-14 11:00:43 +0100

    Documentation

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,4 @@
-# Serializers define the API representation.
+p# Serializers define the API representation.
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
@@ -7,18 +7,21 @@ from pictures.models import Picture
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    """ Serializer for the :model:`django.contrib.auth.models.User` """
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'is_staff')
 
 
 class StudentSerializer(serializers.HyperlinkedModelSerializer):
+    """ Serializer for the :model:`education.models.Student` """
     class Meta:
         model = Student
         fields = ('url', 'user')
 
 
 class PictureSerializer(serializers.HyperlinkedModelSerializer):
+    """ Serializer for the :model:`pictures.models.Picture` """
     class Meta:
         model = Picture
         fields = ('url', 'picture', 'student')
